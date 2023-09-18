087c13b489b564df59732d3ca6fde7ae527412e5
Phumin Decoknoi <139684701+PhuminDecOKnoi@users.noreply.github.com>
2023-09-18 20:59:42 +0700

    Create querySQLByMe.sql

---
+++ querySQLByMe.sql
@@ -0,0 +1,16 @@
+-- INNER JOIN
+-- SELECT `ProductName`,`Quantity`,`Discount` 
+-- FROM test.products 
+-- INNER JOIN test.orderdetails 
+-- ON products.ProductID=orderdetails.ProductID
+-- WHERE test.orderdetails.Discount=0;
+-- --COUNT INNER JOIN
+-- SELECT COUNT(`Quantity`)
+-- FROM test.products 
+-- INNER JOIN test.orderdetails 
+-- ON products.ProductID=orderdetails.ProductID
+-- WHERE test.orderdetails.Discount=0;
+
+--IN
+-- SELECT * FROM test.products
+-- WHERE ProductName IN (`Konbu`,`Gravad lax`);
