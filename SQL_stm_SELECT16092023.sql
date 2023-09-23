dc99c30268f3e186513f51942dc6f997d8c881aa
Phumin Decoknoi <139684701+PhuminDecOKnoi@users.noreply.github.com>
2023-09-23 10:48:45 +0700

    Update SQL_stm_SELECT16092023.sql

--- SQL_stm_SELECT16092023.sql
+++ SQL_stm_SELECT16092023.sql
@@ -3,12 +3,12 @@
 -- SELECT * FROM test.employees;
 -- SELECT EmployeeID,TitleOfCourtesy,FirstName,LastName,Title,country
 -- FROM test.employees;
--- SELECT EmployeeID,CONCAT(TitleOfCourtesy," ",FirstName," ",LastName),country FROM test.employees;
--- SELECT productid, productname, categoryid, unitprice FROM test.products;
--- SELECT productid, productname, categoryid, unitprice, unitsinstock FROM test.products WHERE unitsinstock=0;
--- SELECT productid, productname, categoryid, unitprice, unitsinstock FROM test.products WHERE unitsinstock<20;
--- SELECT productid, productname, categoryid, unitprice, unitsinstock FROM test.products WHERE categoryid=1 or categoryid=8;
--- SELECT productid, productname, categoryid, unitprice, unitsinstock FROM test.products WHERE categoryid IN (1,5,8);
--- SELECT * FROM test.customers WHERE `Country`= "USA";
--- SELECT * FROM test.customers WHERE `Country`= "UK" or `Country`="USA";
--- SELECT * FROM test.customers WHERE `Country`IN ("UK","USA");
+SELECT EmployeeID,CONCAT(TitleOfCourtesy," ",FirstName," ",LastName),country FROM test.employees;
+SELECT productid, productname, categoryid, unitprice FROM test.products;
+SELECT productid, productname, categoryid, unitprice, unitsinstock FROM test.products WHERE unitsinstock=0;
+SELECT productid, productname, categoryid, unitprice, unitsinstock FROM test.products WHERE unitsinstock<20;
+SELECT productid, productname, categoryid, unitprice, unitsinstock FROM test.products WHERE categoryid=1 or categoryid=8;
+SELECT productid, productname, categoryid, unitprice, unitsinstock FROM test.products WHERE categoryid IN (1,5,8);
+SELECT * FROM test.customers WHERE `Country`= "USA";
+SELECT * FROM test.customers WHERE `Country`= "UK" or `Country`="USA";
+SELECT * FROM test.customers WHERE `Country`IN ("UK","USA");
